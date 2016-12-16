c18b06b7527e3484d7cb0dae0f9bb99e441c1473
divramod <divramod@gmail.de>
2016-12-16 12:03:28 +0100

    4.angular2-webpack-starter: clone and copy

--- app/main.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="../jspm_packages/npm/@types/hammerjs@2.0.33/index.d.ts" />
-import 'systemjs-hot-reloader/default-listener.js'
-import "../shims.js"
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule }              from './app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule);
